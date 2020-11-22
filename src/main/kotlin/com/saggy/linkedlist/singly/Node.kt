09745b42789b3e2128e59e81dcee193e21b1c6f3
SagarBalai <sagra10feb@gmail.com>
2020-11-22 12:50:58 +0100

    passing test - 2 node list

--- src/main/kotlin/com/saggy/linkedlist/singly/Node.kt
+++ src/main/kotlin/com/saggy/linkedlist/singly/Node.kt
@@ -1,10 +1,15 @@
 package com.saggy.linkedlist.singly
 
-open class Node(val data:Int) {
-    var next :Node? =null
-    private val length =1
+open class Node(val data: Int) {
+    var next: Node? = null
 
     fun length(): Int {
+        var length = 0
+        var head :Node?= this
+        while (head != null) {
+            length++
+            head = head.next
+        }
         return length
     }
 }
