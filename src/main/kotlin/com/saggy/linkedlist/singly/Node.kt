3340b3d109f913ba22839ca6d208eed1ab0c1ab4
SagarBalai <sagra10feb@gmail.com>
2020-11-22 14:40:35 +0100

    Linked list delete, get and add

--- src/main/kotlin/com/saggy/linkedlist/singly/Node.kt
+++ src/main/kotlin/com/saggy/linkedlist/singly/Node.kt
@@ -1,15 +1,52 @@
 package com.saggy.linkedlist.singly
 
-open class Node(val data: Int) {
+class Node(private val data: Int) {
     var next: Node? = null
+    var head: Node? = this
 
     fun length(): Int {
         var length = 0
-        var head :Node?= this
-        while (head != null) {
+        var temp: Node? = head
+        while (temp != null) {
             length++
-            head = head.next
+            temp = temp.next
         }
         return length
+    }
+
+    fun add(element: Int): Boolean {
+        val temp = Node(element)
+        if (head == null) {
+            head = temp
+        }
+        temp.next = head
+        head = temp
+        return true
+    }
+
+    fun delete(): Boolean {
+        var cur = head
+        var next = cur!!.next
+        while (next != null) {
+            cur!!.next = null
+            cur = next
+            next = next.next
+        }
+        head = null
+        return true
+    }
+
+    fun get(index: Int): Int {
+        var i = 0
+
+        var temp = head
+        while (temp != null) {
+            if (i == index) {
+                return temp.data
+            }
+            temp = temp.next
+            i++
+        }
+        throw IndexOutOfBoundsException("Index $index is not in linked list range")
     }
 }
--- src/test/kotlin/com/saggy/linkedlist/singly/NodeTest.kt
+++ src/test/kotlin/com/saggy/linkedlist/singly/NodeTest.kt
@@ -1,7 +1,9 @@
 package com.saggy.linkedlist.singly
 
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class NodeTest {
     @Test
@@ -13,7 +15,7 @@ internal class NodeTest {
         val length = node.length()
 
         //then
-        assertEquals(1,length)
+        assertEquals(1, length)
     }
 
     @Test
@@ -26,8 +28,53 @@ internal class NodeTest {
         val length = node.length()
 
         //then
-        assertEquals(2,length)
+        assertEquals(2, length)
     }
 
+    @Test
+    internal fun `add - should insert element at first position`() {
+        // given
+        val node = Node(1)
 
+        // when
+        val flag = node.add(2)
+
+        // then
+        assertTrue(flag)
+        assertEquals(2, node.length())
+        assertEquals(2, node.get(0))
+    }
+
+    @Test
+    internal fun `get - should get IndexOutOfBound exception when get element at index greater than list length`() {
+        // given
+        val node = Node(1)
+        node.add(2)
+        node.add(3)
+        node.add(4)
+        node.add(5)
+        val index = node.length() + 1
+
+        // when
+        val result = assertThrows(IndexOutOfBoundsException::class.java) { node.get(index) }
+
+        // then
+        assertEquals("Index $index is not in linked list range", "" + result.message)
+    }
+
+    @Test
+    internal fun `delete - should return true`() {
+        // given
+        val node = Node(1)
+        node.add(2)
+        node.add(3)
+        node.add(4)
+        node.add(5)
+
+        // when
+        node.delete()
+
+        // then
+        assertEquals(0, node.length())
+    }
 }
